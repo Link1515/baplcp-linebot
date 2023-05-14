a4aac0e31e53a7deb078d1db199a3a6d9ab1b709
Link <h26281660@gmail.com>
2023-05-14 10:51:23 +0800

    try mongodb connection

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import { prisma } from './prisma'
 
 import { reloadSchedule } from './schedule'
 import { webhookRoute, sendRoute } from './routes'
 
+;(async () => await prisma.$connect())()
 reloadSchedule()
 
 const app = express()
