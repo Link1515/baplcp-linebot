84d4a81f76a7f5701b42451ec5d481ebcd24d380
Link <h26281660@gmail.com>
2023-05-17 19:35:52 +0800

    remove schedule job

--- index.ts
+++ index.ts
@@ -1,13 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import { prisma } from './prisma'
 
-import { reloadSchedule } from './schedule'
 import { webhookRoute, sendRoute } from './routes'
-
-;(async () => await prisma.$connect())()
-reloadSchedule()
 
 const app = express()
 const PORT = process.env.PORT || 3000
--- routes/sendRoute.ts
+++ routes/sendRoute.ts
@@ -1,10 +1,6 @@
 import express from 'express'
-import { scheduleJob } from 'node-schedule'
 import { z } from 'zod'
-import { prisma } from '../prisma'
-import { ErrorWithCode, badRequestError } from '../errors'
 import { sendMessageToGroup } from '../utils'
-import { setSchedule } from '../schedule'
 
 export const sendRoute = express.Router()
 
@@ -15,39 +11,21 @@ sendRoute.post('/', async (req, res) => {
         message: z.string({
           required_error: 'message is required.',
           invalid_type_error: 'message must be a string.'
-        }),
-        sendAt: z
-          .string({
-            invalid_type_error: 'sendAt must be a date string.'
-          })
-          .optional()
+        })
       },
       { required_error: 'Request body is required.' }
     )
 
-    const { message, sendAt } = bodyValidator.parse(req.body)
+    const { message } = bodyValidator.parse(req.body)
 
-    if (sendAt) {
-      const dateTime = new Date(sendAt)
-      if (isNaN(dateTime.getTime()))
-        throw badRequestError('Invalid Date (sendAt)')
-
-      setSchedule({ dateTime, message })
-    } else {
-      sendMessageToGroup(message)
-    }
-
-    await prisma.$disconnect()
+    sendMessageToGroup(message)
 
     return res.status(200).send('OK')
   } catch (error) {
-    await prisma.$disconnect()
     if (error instanceof z.ZodError) {
       return res
         .status(400)
         .send(error.issues.map((issue) => issue.message).join(' '))
-    } else if (error instanceof ErrorWithCode) {
-      return res.status(error.code).send(error.message)
     }
 
     return res.status(500).send('server error')
