155331e36f75fa8e9cf4d4bee577237431466219
Link <link.lin@udngroup.com>
2023-06-01 16:48:51 +0800

    send message by line message object

--- routes/sendRoute.ts
+++ routes/sendRoute.ts
@@ -1,31 +1,17 @@
 import express from 'express'
-import { z } from 'zod'
 import { sendMessageToGroup } from '../utils'
+import { HTTPError } from '@line/bot-sdk'
 
 export const sendRoute = express.Router()
 
-sendRoute.post('/', async (req, res) => {
+sendRoute.post('/group', async (req, res) => {
   try {
-    const bodyValidator = z.object(
-      {
-        message: z.string({
-          required_error: 'message is required.',
-          invalid_type_error: 'message must be a string.'
-        })
-      },
-      { required_error: 'Request body is required.' }
-    )
-
-    const { message } = bodyValidator.parse(req.body)
-
-    await sendMessageToGroup(message)
+    await sendMessageToGroup(req.body)
 
     return res.status(200).send('OK')
   } catch (error) {
-    if (error instanceof z.ZodError) {
-      return res
-        .status(400)
-        .send(error.issues.map((issue) => issue.message).join(' '))
+    if (error instanceof HTTPError) {
+      return res.status(error.statusCode).send(error.message)
     }
 
     return res.status(500).send('server error')
--- utils/sendMessageToGroup.ts
+++ utils/sendMessageToGroup.ts
@@ -1,12 +1,10 @@
+import { Message } from '@line/bot-sdk'
 import { client } from '../plugins/linebot'
 
-export const sendMessageToGroup = async (message: string) => {
+export const sendMessageToGroup = async (Message: Message) => {
   const groupId = process.env.GROUP_ID
 
   if (!groupId) throw Error('Cannot get groupId')
 
-  await client.pushMessage(groupId, {
-    type: 'text',
-    text: message
-  })
+  await client.pushMessage(groupId, Message)
 }
